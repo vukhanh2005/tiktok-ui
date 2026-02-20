f9577a8559e6dc739a0685c36b3b603c1dffbf19
eni <nvk12a4@gmail.com>
2026-02-20 17:02:21 +0700

    css default layout

--- src/Layout/DefaultLayout/Sidebar/index.js
+++ src/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 function Sidebar() {
-    return ( <h2>Sidebar</h2> );
+    return ( 
+        <aside className={cx('wrapper')}>
+            Sidebar
+        </aside>
+        
+    );
 }
 
 export default Sidebar;
